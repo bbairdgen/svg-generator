1a9658c511d379c3be5ac1bb551549f59f50deb0
bbairdgen <bbairdgen@gmail.com>
2023-02-21 09:43:26 -0700

    adding first attempt

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
 const fs = require('fs').promises
+const inquirer = require('inquirer')
+const shapes = require('./lib/shapes')
 
-fs.writeFile()
+
+const questions = [
+{
+    type: 'maxlength-input',
+    name: 'svgText',
+    message: 'Enter your text (max limit of 3 characters',
+    maxLength: 3
+},
+{
+    type: 'input',
+    name: 'textColor',
+    message: 'Enter your text color choice. If you use a hexadecimal number please include # before the number.'
+},
+{
+    type: 'rawlist',
+    name: 'shapeType',
+    message: 'Pick the shape you would like your logo.',
+    choices: ['Circle', 'Triangle', 'Square']
+},
+{
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Enter your shape color choice. If you use a hexadecimal number please include # before the number.'
+}
+];
+
+function writeToFile() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        // console.log(answers);
+        const circle = shapes.Circle.createCircle(answers)
+        // const svgAnswers = answers
+        // console.log(shapes.startTag());
+        // console.log(shapes.endTag());
+        // fs.writeFile('./examples/logo.svg', circle, (err) =>
+        //     err ? console.log(err) : console.log('Generated logo.svg'))
+    })
+}
+
+// function shapeDecision() {
+//     const createCircle = shapes.createCircle(answers)
+//     if (answers.shapeType === 'Circle') {
+//         createCircle(answers)
+//     }
+// }
+
+writeToFile()
