4d1a87b5f1c01d25e685b8b2ca5a00c44fbebe4a
bbairdgen <bbairdgen@gmail.com>
2023-02-23 13:03:38 -0700

    adding my comments to the file.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const fs = require('fs').promises
+//Variables used to run inquirer and the write file function.
 const inquirer = require('inquirer')
-const { Circle, Triangle, Square } = require('./lib/shapes')
-const SVG = require('./lib/svg')
 const { writeFile } = require('fs/promises');
 
+// variables to call the code written in the mentioned files
+
+const { Circle, Triangle, Square } = require('./lib/shapes')
+const SVG = require('./lib/svg')
+
+//Inquirer questions
 const questions = [
     {
         type: 'maxlength-input',
@@ -29,33 +33,48 @@ const questions = [
     }
 ];
 
+//Function created to call inquirer questions and create the file. 
 function writeToFile() {
     inquirer.prompt(questions)
         .then((answers) => {
             let shape;
+            //Creates an instance of the SVG class
             const svg = new SVG()
+            //This is an easier way to do if-else statements when choices are needed.
             switch (answers.shapeType) {
                 case "Square":
+                    //Creates a new instance of the Square class, when Square is selected.
                     shape = new Square();
+                    //passes arguments to the SVG class
                     svg.renderText(answers.svgText, answers.textColor, 33, 55, 5)
                     break;
                 case "Circle":
+                    //Creates a new instance of the Circle class, when circle is selected.
                     shape = new Circle();
+                    //passes arguments to the SVG class
                     svg.renderText(answers.svgText, answers.textColor, 50, 60, 4.8)
                     break;
-                    default: 
+                default:
+                    //Creates a new instance of the triangle class, when triand is selected or if nothing is selected.
                     shape = new Triangle();
+                    //passes arguments to the SVG class
                     svg.renderText(answers.svgText, answers.textColor, 50, 70, 2.7)
                     break;
             }
+            // passes arugments to the setColor method in the Shape class.
             shape.setColor(answers.shapeColor)
 
-            console.log(shape)
+            // console.log(shape)
+            // Renders the actual shape
             svg.renderShape(shape)
+            // creates the file in the desired location.
             return writeFile('./examples/logo.svg', svg.renderSvg())
         })
+        // success message if the file is created.
         .then(() => console.log('success'))
+        // error message if the file is not created.
         .catch((err) => console.log(err))
 }
 
+//initiates the function when node index.js is called in CLI.
 writeToFile()
